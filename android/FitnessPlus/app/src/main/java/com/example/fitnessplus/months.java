73ec95d171c077394a100f8f290dfcfb0db1e664
PGilSousa <113899335+PGilSousa@users.noreply.github.com>
2023-01-10 17:37:43 +0000

    fixed some buttons

--- android/FitnessPlus/app/src/main/java/com/example/fitnessplus/months.java
+++ android/FitnessPlus/app/src/main/java/com/example/fitnessplus/months.java
@@ -2,7 +2,9 @@ package com.example.fitnessplus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class months extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class months extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_months);
+    }
+
+    public void sendMain20(View view) {
+        Intent intent=new Intent(this,lowerbody.class);
+        startActivity(intent);
+    }
+
+    public void sendMain21(View view) {
+        Intent intent=new Intent(this,UpperBody.class);
+        startActivity(intent);
     }
 }
